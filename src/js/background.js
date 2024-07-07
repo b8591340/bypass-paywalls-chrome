0d08dc5c4ea4226df121a555e069831c3a6005b8
b8591340 <70052269+b8591340@users.noreply.github.com>
2024-07-07 23:03:55 +0200

    Fix Washington Post

--- src/js/background.js
+++ src/js/background.js
@@ -233,7 +233,7 @@ const blockedRegexes = {
   'japantimes.co.jp': /cdn\.cxense\.com\//,
   'scmp.com': /(\.tinypass\.com\/|cdn\.ampproject\.org\/v\d\/amp-access-.+\.js)/,
   'ilmessaggero.it': /(utils\.cedsdigital\.it\/js\/PaywallMeter\.js)/,
-  'washingtonpost.com': /washingtonpost\.com/.*?tetro/,
+  'washingtonpost.com': /\.washingtonpost\.com\/.*?tetro(\..*?js|\/metering)/,
   'theatlantic.com': /cdn\.theatlantic\.com\/_next\/static\/chunks\/pages\/.+\/archive\//,
   'bloomberg.com': /(\.cm\.bloomberg\.com\/|assets\.bwbx\.io\/s\d\/javelin\/.+\/transporter\/)/
 };
